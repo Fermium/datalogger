21edcf3d951d0b0b6a7c3e7a541b6f3f020553b7
sfermiumlabs <s@fermiumlabs.com>
2016-11-01 18:22:44 +0100

    WIP, added basic interface and writing readings to disk

---
+++ renderer.js
@@ -0,0 +1,39 @@
+var dateFormat = require('dateformat'); //for date
+var low = require('lowdb'); //real time .json writing
+var _ = require('lodash');
+var fs = require('fs');
+
+var filename= 'test.json' //development thing
+var file = './data/'+dateFormat(Date.now(), "yyyy_mm_dd_")+filename;//development thing
+var reading = {'series': null,'timestamp': 0, 'value': 0};
+
+const db = low(file);
+db.defaults({ _data : [] , _experiment : '', _date : ''}).value();
+db.set('_experiment','test').value();
+db.set('_date',dateFormat(Date.now(), "yyyy mm dd")).value();
+$('#records').find('thead')
+  .append($('<tr>')
+    .append($('<th>')
+      .text('timestamp'))
+    .append($('<th>')
+      .text('value'))
+  );
+
+setInterval(function(){
+
+/*fake reading*/
+reading.series = 'test';
+reading.timestamp = Date.now();
+reading.value = Math.random();
+////////////////////////////////
+
+db.get('_data').push(reading).value();
+$('#records').find('tbody')
+  .append($('<tr>')
+    .append($('<td>')
+      .text(reading.timestamp))
+    .append($('<td>')
+      .text(reading.value))
+  );
+
+},500);
