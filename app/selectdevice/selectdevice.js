c8795fe3c30f2d035c21dc832b3b12bba3c0a90b
sfermiumlabs <s@fermiumlabs.com>
2017-07-27 18:46:45 +0200

    commit

--- app/selectdevice/selectdevice.js
+++ app/selectdevice/selectdevice.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const {ipcRenderer} = require('electron');
 const jsyaml = require('js-yaml');
-
+app.getCurrentWindow().toggleDevTools();
 $(document).keydown(function(e) {
     // ESCAPE key pressed
     if (e.keyCode == 27) {
@@ -12,7 +12,7 @@ $(document).keydown(function(e) {
     }
 });
 $(document).ready(function(){
-  var producers = getDirectories( (__dirname+'/../devices/').replace('app.asar','app.asar.unpacked'));
+  var producers = getDirectories( (__dirname+'/devices/').replace('app.asar','app.asar.unpacked').replace('/selectdevice',''));
   producers.forEach(function(pr){
     updateList(pr);
   });
@@ -25,21 +25,21 @@ function getDirectories (srcpath) {
 }
 
 function updateList(producer){
-  var products = getDirectories(  (__dirname+'/../devices/'+producer).replace('app.asar','app.asar.unpacked'));
+  var products = getDirectories(  (__dirname+'/devices/'+producer).replace('app.asar','app.asar.unpacked').replace('/selectdevice',''));
   products.forEach(function(x){
     console.log(producer+" - "+x);
     appendProduct(producer,x);
   });
 }
 function appendProduct(producer,name){
-  var product=jsyaml.safeLoad(fs.readFileSync( ( __dirname+'/../devices/'+producer+'/'+name+'/config.yaml').replace('app.asar','app.asar.unpacked'))).product;
+  var product=jsyaml.safeLoad(fs.readFileSync( ( __dirname+'/devices/'+producer+'/'+name+'/config.yaml').replace('app.asar','app.asar.unpacked').replace('/selectdevice',''))).product;
   $('.content-wrapper').append($('<div/>').addClass('col-xs-6').append($('<div/>').addClass('panel panel-default product').attr({
     'style':"-webkit-app-region: no-drag",
     'data-product':name,
     'data-producer':producer,
   })));
   $('[data-product='+name+'][data-producer='+producer+']').append($('<img/>').addClass('panel-heading').attr({
-    src :  (__dirname+'/../devices/'+producer+'/'+name+'/'+product.image).replace('app.asar','app.asar.unpacked'),
+    src :  (__dirname+'/devices/'+producer+'/'+name+'/'+product.image).replace('app.asar','app.asar.unpacked').replace('/selectdevice',''),
     alt : product.name
   }));
   $('[data-product='+name+'][data-producer='+producer+']').append($('<div/>').addClass('panel-body'));
@@ -48,7 +48,7 @@ function appendProduct(producer,name){
   $('[data-product='+name+'][data-producer='+producer+']'+" .panel-body").append($('<p/>').text(product.description));
   $('[data-product='+name+'][data-producer='+producer+']'+" .panel-body").append($('<a/>').addClass('btn btn-primary select-device').attr({
     href : '#',
-    'data-device' :   (__dirname+'/../devices/'+producer+'/'+name+'/').replace('app.asar','app.asar.unpacked')
+    'data-device' :   (__dirname+'/devices/'+producer+'/'+name+'/').replace('app.asar','app.asar.unpacked').replace('/selectdevice','')
   }).text('Next'));
   $('.select-device').each(function(x){
 
