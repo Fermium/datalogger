528092ab0951740c55657e1e8aab1d40f168bfd0
Wilco Tubog <tubog0437@gmanmi.com>
2017-08-24 13:50:19 +0800

    added styling to gain

--- app/main.ts
+++ app/main.ts
@@ -149,7 +149,7 @@ function createPlotWindow (name) {
     height:600,
     title:name,
     show:false,
-    minWidth: 810,
+    minWidth: 800,
     minHeight: 600
   });
   plotWindow[name].loadURL(`file://${__dirname}/plot/index.html`);
@@ -177,7 +177,7 @@ app.on('ready', function(){
     height: 950,
     show:false,
     backgroundColor:'#f5f5f5',
-    minWidth: 810,
+    minWidth: 800,
     minHeight: 600
   });
   createSelectDevice();
