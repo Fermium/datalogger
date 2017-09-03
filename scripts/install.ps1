118cbcc9cfdf70f58f326355f3742192b7fe40fe
Davide Bortolami <d@fermiumlabs.com>
2017-09-03 14:06:58 +0200

    FIX FUCKING WINDOWS BUILDS

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -2,10 +2,10 @@ cinst -y nodejs-lts
 refreshenv
 node -v
 cinst -y yarn --ignore-dependencies
-cinst -y python2
-cinst -y visualstudioexpress2013windowsdesktop
+#cinst -y python2
+#cinst -y visualstudioexpress2013windowsdesktop
 #cint -y microsoft-build-tools-2013
 #cinst -y visualstudiocode
 #choco install vcbuildtools -y
-#npm install -g windows-build-tools
+npm install -g --production windows-build-tools
 refreshenv
